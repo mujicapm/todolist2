b07754a65b726b8297ca95335dcff732a79f09b4
Mauro III <mauro.mujica.3@gmail.com>
2021-10-21 06:09:26 -0500

    Successfully implemented Axios. On step 5 now.

--- src/App.js
+++ src/App.js
@@ -14,38 +14,12 @@ function App() {
         method: 'get'
     }))
 
-  // const initialToDoItems = [
-  //   {
-  //     UUID: "223423",
-  //     title: "Task 1",
-  //     description: "Some text",
-  //     dateCreated: "5/10/2021",
-  //     isComplete: false,
-  //     dateComplete: "",
-  //   },
-  //   {
-  //     UUID: "22493423",
-  //     title: "Task 2",
-  //     description: "Some text",
-  //     dateCreated: "6/10/2021",
-  //     isComplete: false,
-  //     dateComplete: "",
-  //   },
-  //   {
-  //     UUID: "22993423",
-  //     title: "Task 3",
-  //     description: "Some text",
-  //     dateCreated: "8/8/2021",
-  //     isComplete: false,
-  //     dateComplete: "",
-  //   },
-  // ];
     const [ state, dispatch ] = useReducer(appReducer, { user: '', ToDoItems: [] })
 
     useEffect(getToDoItems, [])
     useEffect(() => {
         if (ToDoItems && ToDoItems.data) {
-            dispatch({ type: 'FETCH_POSTS', posts: ToDoItems.data })
+            dispatch({ type: 'FETCH_POSTS', ToDoItems: ToDoItems.data.reverse() })
         }
     }, [ToDoItems])
 
--- src/ToDos/CreateToDoItem.js
+++ src/ToDos/CreateToDoItem.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from "react";
 import { v4 } from "uuid";
 import handleDateCreated from "../HandleDate";
 import { StateContext } from '../Contexts';
+import {useResource} from "react-request-hook";
 
 export default function CreatePost({}) {
     const{dispatch} = useContext(StateContext);
@@ -9,6 +10,18 @@ export default function CreatePost({}) {
     const [description, setDescription] = useState("");
     const [dateCreated, setDateCreated] = useState("");
     const [UUID, setUUID] = useState("");
+
+    // const [ ToDoItem, putToDoItem ] = useResource(({UUID, title, description, dateCreated}) => ({
+    //     url: '/ToDoItems',
+    //     method: 'post',
+    //     data:
+    //         {
+    //             UUID,
+    //             title,
+    //             description,
+    //             dateCreated
+    //         }
+    // }))
 
     function handleTitle(evt) {
         setTitle(evt.target.value);
