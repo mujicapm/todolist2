31df19807666cfc4c9ee6153f5017be53adf6780
mujicapm <mauro.mujica.3@gmail.com>
2021-11-20 09:38:07 -0600

    Bootstrap React Installed. Part I not complete. Pages directory, headerbar, and homepage introduced. Functionally working as expected. Need to introduce individuals todoitem as a page and then introduce styling.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import { StateContext } from './Contexts';
 import {Col, Container, Row} from "react-bootstrap";
 import HeaderBar from "./pages/HeaderBar";
 import HomePage from "./pages/HomePage";
+import CreateToDoItem from "./ToDos/CreateToDoItem"
+import {mount, route} from "navi";
+import TodoPage from "./pages/ToDoPage";
 
 
 function App() {
 
     const [ state, dispatch ] = useReducer(appReducer, { user: '', ToDoItems: [] })
 
-    return (
+    const routes = mount({
+        '/': route({view: <HomePage/>}),
+        '/todo/create': route({view: <CreateToDoItem/>}),
+        '/todo/:id': route(req => {
+            return {view: <TodoPage id={req.params.id}/>}
+        }),
+    })
+
+    //TODO On Slide "Defining Routes" - 1:19:50 10/19 Only implmented top left
+
+
+
+return (
       <div>
         <StateContext.Provider value={{state: state, dispatch: dispatch}}>
             <Container>
